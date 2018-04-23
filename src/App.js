22c986a284c73a232d4d282d90454ec51a50b449
Michelle Colon <michellecolon.mc@gmail.com>
2018-04-23 00:02:02 -0400

    api key and call done- form set up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from "react";
 
 import Titles from "./components/Titles";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
+
+const API_KEY = "0b9d912312a65ff49746eb20471eeabc";
+
 
 class App extends React.Component{
+  getWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=imperial`);
+    const data = await api_call.json();
+    console.log(data);
+  }
   render(){
     return(
       <div>
         <Titles />
+        <Form getWeather= {this.getWeather}/>
+        <Weather />
       </div>
     );
   }
